7a033b9c0ee06796abb527afe9f6800476e50cca
yunus_ibadov <ibadovunus55@gmail.com>
2023-11-15 14:19:22 +0500

    fixed

--- pages/card_page/js/chart_custom.js
+++ pages/card_page/js/chart_custom.js
@@ -3,6 +3,9 @@ import { getData } from '../../../modules/helpers';
 let canvas_tag = document.getElementById('acquisitions')
 let categ_canvas = document.getElementById('categories')
 let today_btn = document.querySelector('#today')
+let week_btn = document.querySelector('#week')
+let month_btn = document.querySelector('#month')
+let year_btn = document.querySelector('#year')
 let id = location.search.split('=').at(-1)
 let transactions = []
 let categories = []
@@ -12,31 +15,93 @@ let categ_chart
 getData('/transactions?wallet_id=' + id)
     .then(res => {
         transactions = res.data
-        for(let item of res.data) {
+        for (let item of res.data) {
             categories.push(item.category)
         }
 
         categoriesView(res.data)
         createChart(res.data)
-    })  
+    })
 
 today_btn.onclick = () => {
     let todays_tr = []
-    let date_now = new Date().toISOString().substring(0,10)
+    let date_now = new Date().toISOString().substring(0, 10)
 
-    for(let item of transactions) {
-        if(item.date === date_now) {
+    for (let item of transactions) {
+        if (item.date === date_now) {
+            todays_tr.push(item)
+        }
+        
+
+    }
+
+
+categoriesView(todays_tr)
+        createChart(todays_tr)
+}
+
+week_btn.onclick = () => {
+    let todays_tr = []
+    let date_now = new Date().toISOString().substring(0, 10)
+    let week = [date_now.split('-').at(-1), date_now.split('-').at(-1) - 1, date_now.split('-').at(-1) - 2, date_now.split('-').at(-1) - 3, date_now.split('-').at(-1) - 4, date_now.split('-').at(-1) - 5, date_now.split('-').at(-1) - 6, date_now.split('-').at(-1) - 7]
+
+
+
+
+
+    for (let item of transactions) {
+        for (let i of week) {
+            if (+i == item.date.split('-').at(-1)) {
+                todays_tr.push(item)
+            }       
+        }
+    }
+
+   
+ categoriesView(todays_tr)
+ createChart(todays_tr)
+}
+
+month_btn.onclick = () => {
+    let todays_tr = []
+    let date_now = new Date().toISOString().substring(0, 10)
+    let month = date_now.split('-').at(-2)
+
+
+    for (let item of transactions) {
+        if (item.date.split('-').at(-2) === month) {
             todays_tr.push(item)
         }
     }
 
     categoriesView(todays_tr)
     createChart(todays_tr)
-    
+
 }
 
+year_btn.onclick = () => {
+    let todays_tr = []
+    let date_now = new Date().toISOString().substring(0, 10)
+    let year = date_now.split('-').at(-3)
+
+
+    for (let item of transactions) {
+        if (item.date.split('-').at(-3) === year) {
+            todays_tr.push(item)
+        }
+    }
+
+    categoriesView(todays_tr)
+    createChart(todays_tr)
+
+}
+
+
+
+
+
 function createChart(arr) {
-    if(myChart) {
+    if (myChart) {
         myChart.destroy()
     }
 
@@ -53,7 +118,7 @@ function createChart(arr) {
 }
 
 function categoriesView(arr) {
-    if(categ_chart) {
+    if (categ_chart) {
         categ_chart.destroy()
     }
 
@@ -66,5 +131,5 @@ function categoriesView(arr) {
                 data: arr.map(item => item.amount),
             }]
         },
-    });   
+    });
 }
--- pages/wallets/index.js
+++ pages/wallets/index.js
@@ -1,25 +1,9 @@
-<<<<<<< HEAD
-import { reload_card, makeHeader } from "../../modules/ui";
-=======
 // import axios from 'axios'
 import { reload_card, makeHeader } from "../../modules/ui"
->>>>>>> dcb146f688934e206d390c47c467fe424beb3153
 import { getData } from '../../modules/helpers';
 import { user } from "../../modules/user";
-import axios from "axios";
-const bas_url ="http://localhost:8080"
-
 
 let container = document.querySelector('.cart')
-let form = document.querySelector('.add')
-let open = document.querySelector('.button')
-let card_name = document.querySelector("#name")
-let card_current = document.querySelector("#current")
-
-axios(bas_url + "/wallets")
-    .then(function (res) {
-        reload_card(res.data, container)
-    });
 
 makeHeader()
 
@@ -31,9 +15,4 @@ user_emails.forEach(a => a.innerHTML = user.email)
 
 
 getData('/wallets?user_id=' + user.id)
-<<<<<<< HEAD
-    .then(res => console.log(res.data))
-=======
     .then(res => reload_card(res.data, container))
-
->>>>>>> dcb146f688934e206d390c47c467fe424beb3153
